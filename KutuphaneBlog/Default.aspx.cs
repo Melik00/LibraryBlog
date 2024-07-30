ca1e0826c84ad5e9c95e595f6aec0e5f2b584184
Melikcan Faruk Özçiriş <332261016@ogr.uludag.edu.tr>
2024-07-30 15:47:22 +0300

    Veri Tabanı Oluşturma Projeye Entity ekleme ve Router ile veritabanından verileri çekme.

--- KutuphaneBlog/Default.aspx.cs
+++ KutuphaneBlog/Default.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using KutuphaneBlog.Entity;
+
 
 namespace KutuphaneBlog
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        DboGemlikKitabevimEntities db = new DboGemlikKitabevimEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var kitaplar = db.TBL_BLOG.ToList();
+            Repeater1.DataSource = kitaplar;
+            Repeater1.DataBind();
         }
     }
 }
--- KutuphaneBlog/Default.aspx.designer.cs
+++ KutuphaneBlog/Default.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace KutuphaneBlog
 
     public partial class WebForm1
     {
+
+        /// <summary>
+        /// Repeater1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
     }
 }
